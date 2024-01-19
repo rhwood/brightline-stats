772a9822d15f3c9e3e3226bbdf486eb735789752
Randall Wood <297232+rhwood@users.noreply.github.com>
2024-01-19 14:29:16 -0500

    feat: add intro text

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import { PageSection } from '@patternfly/react-core/dist/dynamic/components/Page'
 import { Title } from '@patternfly/react-core/dist/dynamic/components/Title'
 import { MultiYearChart, Ridership } from '@app/utils/MultiYearChart';
+import { Text, TextContent } from '@patternfly/react-core/dist/dynamic/components/Text'
 
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
     <Title headingLevel="h1" size="lg">Total Ridership</Title>
+    <TextContent>
+      <Text component="p">
+        The Virginia Rail Passenger Authority (VRPA) sponsors eight Amtrak trains on four corridors.
+        This site visualizes the ridership data that VRPA publishes.
+      </Text>
+    </TextContent>
     <MultiYearChart lineFn={(row: Ridership): number => row['Newport News'] + row.Norfolk + row.Richmond + row.Roanoke} />
   </PageSection>
 )
