6f25a80b20a049dda0306d26518089930ffd32cc
Randall Wood <297232+rhwood@users.noreply.github.com>
2024-01-20 07:09:20 -0500

    feat: more notes on data

--- src/app/Corridors/Richmond.tsx
+++ src/app/Corridors/Richmond.tsx
@@ -2,11 +2,27 @@ import * as React from 'react';
 import { PageSection } from '@patternfly/react-core/dist/dynamic/components/Page'
 import { Title } from '@patternfly/react-core/dist/dynamic/components/Title'
 import { MultiYearChart, Ridership } from '@app/utils/MultiYearChart';
+import { Text, TextContent, TextList, TextListItem, TextVariants } from '@patternfly/react-core/dist/dynamic/components/Text';
 
 const Richmond: React.FunctionComponent = () => (
   <PageSection>
     <Title headingLevel="h1" size="lg">Richmond Corridor Ridership</Title>
+    <TextContent>
+      <Text component={TextVariants.p}>
+        The Richmond corridor consists of Northeast Regional services terminating at Richmond Main Street station.
+        The entirety of the Richmond corridor is shared with the Newport News corridor, and all of the Richmod
+        corridor except the Richmond Main Street station is shared with the Norfolk corridor.
+      </Text>
+    </TextContent>
     <MultiYearChart lineFn={(row: Ridership): number => row.Richmond} />
+    <TextContent>
+      <Text component={TextVariants.h3}>Notes</Text>
+      <TextList isPlain>
+        <TextListItem>Services only on this corridor was suspended from March 2020 to August 2021.</TextListItem>
+        <TextListItem>Ridership origininating or terminating at stations in this corridor, but on Newport News
+          or Norfolk corridor services are shown under those services.</TextListItem>
+      </TextList>
+    </TextContent>
   </PageSection>
 )
 
--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ const Dashboard: React.FunctionComponent = () => (
     <TextContent>
       <Text component="p">
         The Virginia Rail Passenger Authority (VRPA) sponsors eight Amtrak trains on four corridors.
-        This site visualizes the ridership data that VRPA publishes.
+        This site visualizes the ridership data for those trains as provided monthly by the VRPA.
       </Text>
     </TextContent>
     <MultiYearChart lineFn={(row: Ridership): number => row['Newport News'] + row.Norfolk + row.Richmond + row.Roanoke} />
