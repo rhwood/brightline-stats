343a8a047643d071838f2212bc15a5237b673baa
Randall Wood <297232+rhwood@users.noreply.github.com>
2024-11-23 09:01:20 -0500

    update mock data and snapshots
    Mock data was incomplete enough to capture the weirdness being accounted for in the logic in the rolling charts.

--- __mocks__/monthly-revenue.js
+++ __mocks__/monthly-revenue.js
@@ -1,6 +1,18 @@
 const ridership = [
-    { Year: 1900, Month: 3, short: 2000, long: null, ancillary: 1000 },
-    { Year: 1900, Month: 4, short: 2000, long: 3000, ancillary: 4000 }
+    { Year: 2023, Month: 4, short: 3.4, long: null, ancillary: 1.5 },
+    { Year: 2023, Month: 5, short: 3.5, long: null, ancillary: 1.7 },
+    { Year: 2023, Month: 6, short: 2.7, long: null, ancillary: 1.5 },
+    { Year: 2023, Month: 7, short: 2.8, long: null, ancillary: 1.5 },
+    { Year: 2023, Month: 8, short: 2.6, long: null, ancillary: 1.4 },
+    { Year: 2023, Month: 9, short: 2.6, long: 1.5, ancillary: 1.7 },
+    { Year: 2023, Month: 10, short: 3.1, long: 7.3, ancillary: 2.4 },
+    { Year: 2024, Month: 4, short: 3.5, long: 8.8, ancillary: 2.9 },
+    { Year: 2024, Month: 5, short: 2.9, long: 9.6, ancillary: 2.9 },
+    { Year: 2024, Month: 6, short: 2.4, long: 9.9, ancillary: 2.9 },
+    { Year: 2024, Month: 7, short: 2.1, long: 9.9, ancillary: 2.8 },
+    { Year: 2024, Month: 8, short: 2.0, long: 9.3, ancillary: 2.7 },
+    { Year: 2024, Month: 9, short: 1.9, long: 9.4, ancillary: 2.8 },
+    { Year: 2024, Month: 10, short: 2.3, long: 9.9, ancillary: 2.8 },
 ]
 
 export default ridership
--- __mocks__/monthly-ridership.js
+++ __mocks__/monthly-ridership.js
@@ -1,6 +1,18 @@
 const ridership = [
-    { Year: 1900, Month: 3, short: 2000, long: null, ancillary: null },
-    { Year: 1900, Month: 4, short: 2000, long: 3000, ancillary: null }
+    { Year: 2023, Month: 4, short: 151080, long: null, ancillary: null },
+    { Year: 2023, Month: 5, short: 168137, long: null, ancillary: null },
+    { Year: 2023, Month: 6, short: 149536, long: null, ancillary: null },
+    { Year: 2023, Month: 7, short: 156478, long: null, ancillary: null },
+    { Year: 2023, Month: 8, short: 149821, long: null, ancillary: null },
+    { Year: 2023, Month: 9, short: 125475, long: 17578, ancillary: null },
+    { Year: 2023, Month: 10, short: 126059, long: 79686, ancillary: null },
+    { Year: 2024, Month: 4, short: 103737, long: 119380, ancillary: null },
+    { Year: 2024, Month: 5, short: 95965, long: 135918, ancillary: null },
+    { Year: 2024, Month: 6, short: 84062, long: 139307, ancillary: null },
+    { Year: 2024, Month: 7, short: 76907, long: 143488, ancillary: null },
+    { Year: 2024, Month: 8, short: 71507, long: 136555, ancillary: null },
+    { Year: 2024, Month: 9, short: 75230, long: 130249, ancillary: null },
+    { Year: 2024, Month: 10, short: 83514, long: 134221, ancillary: null },
 ]
 
 export default ridership
