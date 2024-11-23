1b8040608b2ff521a08055a35d6df5823abe5e68
Randall Wood <297232+rhwood@users.noreply.github.com>
2024-11-23 09:02:38 -0500

    fix: rolling data is displayed correctly (#19)
    Fixes #18

--- __mocks__/monthly-fare.js
+++ __mocks__/monthly-fare.js
@@ -1,6 +1,18 @@
 const ridership = [
-    { Year: 1900, Month: 3, short: 20.00, long: null, ancillary: null },
-    { Year: 1900, Month: 4, short: 20.00, long: 30.00, ancillary: null }
+    { Year: 2023, Month: 4, short: 22.81, long: null, ancillary: 22.81 },
+    { Year: 2023, Month: 5, short: 20.63, long: null, ancillary: 20.63 },
+    { Year: 2023, Month: 6, short: 18.10, long: null, ancillary: 18.10 },
+    { Year: 2023, Month: 7, short: 18.01, long: null, ancillary: 18.01 },
+    { Year: 2023, Month: 8, short: 17.23, long: null, ancillary: 17.23 },
+    { Year: 2023, Month: 9, short: 20.63, long: 84.16, ancillary: 28.44 },
+    { Year: 2023, Month: 10, short: 24.53, long: 91.79, ancillary: 50.58 },
+    { Year: 2024, Month: 4, short: 33.51, long: 74.13, ancillary: 55.25 },
+    { Year: 2024, Month: 5, short: 30.62, long: 70.81, ancillary: 54.18 },
+    { Year: 2024, Month: 6, short: 28.03, long: 71.46, ancillary: 55.11 },
+    { Year: 2024, Month: 7, short: 27.08, long: 69.33, ancillary: 54.59 },
+    { Year: 2024, Month: 8, short: 27.76, long: 68.04, ancillary: 54.20 },
+    { Year: 2024, Month: 9, short: 25.88, long: 71.85, ancillary: 55.02 },
+    { Year: 2024, Month: 10, short: 27.60, long: 73.98, ancillary: 56.19 },
 ]
 
 export default ridership
